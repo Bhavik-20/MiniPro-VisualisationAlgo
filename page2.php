69a0ef641ec1e80824a814f1b36023d1221f214d
Piyush-Chavda <piushchavda123@gmail.com>
2021-04-04 20:05:59 +0530

    piyush

--- page2.php
+++ page2.php
@@ -37,6 +37,8 @@
 <style>
 * {
   box-sizing: border-box;
+  color: white;
+   background-image:linear-gradient(black, #01012A);
 }
 
 /* Create two equal columns that floats next to each other */
@@ -44,16 +46,67 @@
   float: left;
   width: 50%;
   padding: 10px;
+  align-content: center;
 }
 
 /* Clear floats after the columns */
 .row:after {
   content: "";
-  display: table;
+ /* display: table;*/
   clear: both;
+  margin-left:60px;
+  align-items: center;
+}
+ input[type=number], select {
+  color: white;
+  width: 10%;
+  padding: 12px 20px;
+  margin: 8px 0;
+/*  display: inline-block;*/
+  border-radius: 10px;
+ /* box-sizing: border-box;*/
+  background-color:#5E6075;
+  border-color:#5E6075;
+  opacity: 75%;
+}
+input[type=submit] {
+  width: 10%;
+background-color: #181B29;
+    border: 2px solid #4CAF50; 
+  align-content: center;
+  color:white;
+  padding: 14px 20px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  margin: auto;
+  margin-top: 15px;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  font-weight: 850;
+}
+.heading
+            {
+                align-content: center;
+                margin-top: 5px;
+                margin-left: 40px;
+                padding-top: 30px;
+                padding-right: 20px;
+                padding-bottom: 10px;
+                padding-left: 20px;
+                
+            }
+
+input[type=submit]:hover {
+  background-color: white;
+  color:grey;
 }
 </style>
 <body>
+     <div class="heading">
+    <img src="headd.png" alt="Girl in a jacket" style="width:1200px;height:100px;">
+</div>
     <form action="page2.php" method="POST">
         <div class="row">
             <div class="column">
@@ -76,9 +129,7 @@
                 <?php } ?>
             </div>
         </div>
-        <div class="center">
             <input type="submit" name="submit" value="Submit">
-        </div>
     </form>
 </body>
 </html>
